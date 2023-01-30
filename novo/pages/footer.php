f9e26c6d1d593fd809b40b2075d92bada0d598fc
DrielySantos <santosdriely98@gmail.com>
2023-01-30 18:01:30 -0300

    PHP

---
+++ novo/pages/footer.php
@@ -0,0 +1,51 @@
+
+
+<div class="container-fluid ">
+        <div class="row text-center">
+          <div class="col-md-4 footer">
+            <h4> Mapa do site</h4>
+            <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Tratamentos</a></li>
+              <li><a href="#">Nossa clínica</a></li>
+              <li><a href="#">Agendamento</a></li>
+            </ul>
+          </div>
+
+          <div class="col-md-4 footer">
+            <h4>Fale conosco</h4>
+            <ul>
+              <li><a class="btn-outline" href="https://web.whatsapp.com/" target="_blank">Atendimento ao Cliente</a></li>
+              <li><a href="https://web.whatsapp.com/" target="_blank">Dúvidas</a></li>
+              <li><a href="https://web.whatsapp.com/" target="_blank">Reclamações</a></li>
+              <li><a href="https://web.whatsapp.com/" target="_blank">Cancelamento</a></li>
+              <li><a href="https://web.whatsapp.com/" target="_blank">Ouvidoria</a></li>
+              <li><a href="mailto:emaildaclinica@gmail.com" target="_blank">E-mail:emaildaclinica@gmail.com</a></li>
+            </ul>
+          </div>
+          
+          <div class="col-md-4 footer">
+            <h4>Acompanhe nas redes sociais</h4>
+                <a href="https://facebook.com/" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-facebook"></i></a>
+                <a href="https://instagram.com/" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-instagram"></i></a>
+                <a href="https://tiktok.com/" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-tiktok"></i></a>
+                <a href="https://web.whatsapp.com/" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-whatsapp"></i></a>
+          </div>
+      
+          
+        </div>
+
+    <!-- JavaScript (Opcional) -->
+    <script src="script.js"></script>
+    <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js" integrity="sha512-XtmMtDEcNz2j7ekrtHvOVR4iwwaD6o/FUJe6+Zq+HgcCsk3kj4uSQQR8weQ2QVj1o0Pk6PwYLohm206ZzNfubg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
