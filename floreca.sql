8d5f78931134566f1ec29d7a5666b11f0ab3294c
DrielySantos <santosdriely98@gmail.com>
2023-02-25 13:00:18 -0300

    novo sql

--- floreca.sql
+++ floreca.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 24-Fev-2023 às 01:56
--- Versão do servidor: 10.4.24-MariaDB
--- versão do PHP: 8.1.6
+-- Tempo de geração: 25-Fev-2023 às 14:07
+-- Versão do servidor: 10.4.25-MariaDB
+-- versão do PHP: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,10 +18,10 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `floreca`
+-- Banco de dados: `florecateste`
 --
-CREATE DATABASE IF NOT EXISTS `floreca` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `floreca`;
+CREATE DATABASE IF NOT EXISTS `florecateste` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `florecateste`;
 
 -- --------------------------------------------------------
 
@@ -38,18 +38,19 @@ CREATE TABLE `cliente` (
   `datanascimento` date NOT NULL,
   `cep` char(9) NOT NULL,
   `numerocasa` smallint(6) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `status` char(1) NOT NULL
+  `email` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Extraindo dados da tabela `cliente`
 --
 
-INSERT INTO `cliente` (`idcliente`, `nome`, `telefone`, `cpf`, `rg`, `datanascimento`, `cep`, `numerocasa`, `email`, `status`) VALUES
-(1, 'William Costa', '(21)97070-7070', '120157142-10', '', '1987-04-15', '23036-060', 900, 'william@gmail.com', 'C'),
-(2, 'Sue Costa', '21989247323', '17548215745', '12578954', '1994-02-15', '23036060', 96, 'sue@gmail.com', ''),
-(3, 'Maria Vaz', '21985474521', '12515412470', '12024589', '1987-04-25', '23036060', 52, 'mariavaz@gmail.com', '');
+INSERT INTO `cliente` (`idcliente`, `nome`, `telefone`, `cpf`, `rg`, `datanascimento`, `cep`, `numerocasa`, `email`) VALUES
+(1, 'William Costa', '(21)97070-7070', '120157142-10', '', '1987-04-15', '23036-060', 900, 'william@gmail.com'),
+(2, 'Sue Costa', '21989247323', '17548215745', '12578954', '1994-02-15', '23036060', 96, 'sue@gmail.com'),
+(3, 'Maria Vaz', '21985474521', '12515412470', '12024589', '1987-04-25', '23036060', 52, 'mariavaz@gmail.com'),
+(4, 'Pedro Rodrigo', '21975854121', '16423025410', '1632048', '1990-03-16', '21512170', 18, 'pedro@gmail.com'),
+(5, 'Caio Luiz', '21985746521', '14715412476', '15425797', '2000-04-15', '21512110', 78, 'caio@outlook.com');
 
 -- --------------------------------------------------------
 
@@ -64,6 +65,13 @@ CREATE TABLE `contacts` (
   `message` text NOT NULL,
   `status` enum('sended','readed','responded','deleted') DEFAULT 'sended'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `contacts`
+--
+
+INSERT INTO `contacts` (`nome`, `telefone`, `email`, `message`, `status`) VALUES
+('João Santos', '21985746526', 'joaosantos@gmail.com', 'Preenchimento Facial', 'sended');
 
 -- --------------------------------------------------------
 
@@ -94,7 +102,7 @@ INSERT INTO `endereco` (`cep`, `rua`, `bairro`, `cidade`, `uf`) VALUES
 --
 
 CREATE TABLE `funcionario` (
-  `idfuncionario` char(14) NOT NULL,
+  `idfuncionario` int(11) NOT NULL,
   `cpffuncionario` char(14) NOT NULL,
   `nome` varchar(60) NOT NULL,
   `telefone` varchar(15) NOT NULL,
@@ -110,9 +118,10 @@ CREATE TABLE `funcionario` (
 --
 
 INSERT INTO `funcionario` (`idfuncionario`, `cpffuncionario`, `nome`, `telefone`, `rg`, `cep`, `numerocasa`, `foto`, `email`) VALUES
-('1', '123', 'Mário Silva', '(21)9999-8888', '0001', '23085-610', 40, 'vazio', ''),
-('2', '456', 'Gabriel Silva', '(21)9999-7777', '0002', '26551-090', 100, 'vazio', ''),
-('3', '789', 'Mariana Souza', '(21)9999-5555', '1234', '23085-610', 1820, 'vazio', '');
+(1, '10112412014', 'Mário Silva', '(21)9999-8888', '0001', '23085-610', 40, NULL, 'mariosilva@outlook.com'),
+(2, '14520135478', 'Gabriel Silva', '(21)9999-7777', '0002', '26551-090', 100, NULL, 'silva@gmail.com'),
+(3, '14715412470', 'Nelly Vasquez', '21985746520', '15425798', '21512110', 80, NULL, 'nelly@outlook.com'),
+(4, '12015612444', 'Diná Costa', '21977776666', '15425793', '23036060', 900, NULL, 'dina@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -124,12 +133,18 @@ CREATE TABLE `itemservico` (
   `iditem` int(11) NOT NULL,
   `idfuncionario` int(11) NOT NULL,
   `idservico` int(11) NOT NULL,
-  `idprodecimento` int(11) NOT NULL,
-  `data` date NOT NULL,
+  `idprocedimento` int(11) NOT NULL,
   `horario` varchar(30) NOT NULL,
   `valor` double NOT NULL,
   `categoria` varchar(60) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `itemservico`
+--
+
+INSERT INTO `itemservico` (`iditem`, `idfuncionario`, `idservico`, `idprocedimento`, `horario`, `valor`, `categoria`) VALUES
+(7, 4, 9, 1, '', 500, 'facial');
 
 -- --------------------------------------------------------
 
@@ -161,10 +176,24 @@ INSERT INTO `procedimento` (`idprocedimento`, `nomeprocedimento`, `descricao`, `
 CREATE TABLE `servico` (
   `idservico` int(11) NOT NULL,
   `data` date NOT NULL,
-  `horario` varchar(30) NOT NULL,
   `valor` double NOT NULL,
   `idcliente` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `servico`
+--
+
+INSERT INTO `servico` (`idservico`, `data`, `valor`, `idcliente`) VALUES
+(1, '2023-02-24', 850, 2),
+(2, '2023-02-24', 850, 2),
+(3, '2023-02-24', 850, 2),
+(4, '2023-02-24', 850, 2),
+(5, '2023-02-24', 850, 2),
+(6, '2023-02-24', 500, 1),
+(7, '2023-02-25', 700, 4),
+(8, '2023-02-25', 500, 5),
+(9, '2023-02-25', 500, 4);
 
 -- --------------------------------------------------------
 
@@ -184,14 +213,6 @@ CREATE TABLE `servico_temp` (
   `nomecli` varchar(60) NOT NULL,
   `nomefunc` varchar(60) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Extraindo dados da tabela `servico_temp`
---
-
-INSERT INTO `servico_temp` (`idcliente`, `idfuncionario`, `idprocedimento`, `data`, `horario`, `valor`, `nomeproced`, `categoria`, `nomecli`, `nomefunc`) VALUES
-(2, 2, 1, '2023-02-15', '22:48', 500, 'Preenchimento Facial', 'facial', 'Sue Costa', 'Gabriel Silva'),
-(2, 2, 2, '2023-02-15', '22:48', 350, 'Radiofrequência', 'corporal', 'Sue Costa', 'Gabriel Silva');
 
 --
 -- Índices para tabelas despejadas
@@ -215,7 +236,8 @@ ALTER TABLE `funcionario`
 ALTER TABLE `itemservico`
   ADD PRIMARY KEY (`iditem`),
   ADD KEY `fk_idservico` (`idservico`),
-  ADD KEY `fk_procedimento` (`idprodecimento`);
+  ADD KEY `fk_procedimento` (`idprocedimento`),
+  ADD KEY `fk_idfuncionario` (`idfuncionario`);
 
 --
 -- Índices para tabela `procedimento`
@@ -231,6 +253,12 @@ ALTER TABLE `servico`
   ADD KEY `fk-cliente` (`idcliente`);
 
 --
+-- Índices para tabela `servico_temp`
+--
+ALTER TABLE `servico_temp`
+  ADD KEY `fk_idprocedimento` (`idprocedimento`);
+
+--
 -- AUTO_INCREMENT de tabelas despejadas
 --
 
@@ -238,13 +266,25 @@ ALTER TABLE `servico`
 -- AUTO_INCREMENT de tabela `cliente`
 --
 ALTER TABLE `cliente`
-  MODIFY `idcliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `idcliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de tabela `funcionario`
+--
+ALTER TABLE `funcionario`
+  MODIFY `idfuncionario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de tabela `itemservico`
 --
 ALTER TABLE `itemservico`
-  MODIFY `iditem` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `iditem` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de tabela `servico`
+--
+ALTER TABLE `servico`
+  MODIFY `idservico` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Restrições para despejos de tabelas
@@ -254,8 +294,9 @@ ALTER TABLE `itemservico`
 -- Limitadores para a tabela `itemservico`
 --
 ALTER TABLE `itemservico`
-  ADD CONSTRAINT `fk_idservico` FOREIGN KEY (`idservico`) REFERENCES `servico` (`idservico`),
-  ADD CONSTRAINT `fk_procedimento` FOREIGN KEY (`idprodecimento`) REFERENCES `procedimento` (`idprocedimento`);
+  ADD CONSTRAINT `fk_idfuncionario` FOREIGN KEY (`idfuncionario`) REFERENCES `funcionario` (`idfuncionario`),
+  ADD CONSTRAINT `fk_procedimento` FOREIGN KEY (`idprocedimento`) REFERENCES `procedimento` (`idprocedimento`),
+  ADD CONSTRAINT `fk_servico` FOREIGN KEY (`idservico`) REFERENCES `servico` (`idservico`);
 
 --
 -- Limitadores para a tabela `servico`
